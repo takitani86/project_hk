6e5175d4a048fd6a3e89e42a489ff4cc38df178a
HKEDU <HKEDU@192.168.5.108>
2019-07-02 14:51:51 +0900

    memberDao, memberService 작성

---
+++ projectOne/src/main/java/com/hk/one/dao/MemberDao.java
@@ -0,0 +1,57 @@
+package com.hk.one.dao;
+
+import java.util.List;
+
+import com.hk.one.dto.MemberDto;
+
+public class MemberDao implements IMemberDao {
+
+	@Override
+	public List<MemberDto> getAllMember() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MemberDto getMember(int mem_seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean joinMember(MemberDto memberDto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteMember(int mem_seq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateMember(MemberDto memberDto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public MemberDto existMember(String mem_id, String mem_pw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkIdMember(String mem_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkEmailMember(String mem_email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
